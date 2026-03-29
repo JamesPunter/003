72eafe6c359b9dbd851fbaf6999134f5584c8083
JamesPunter <jamespunter27@gmail.com>
2026-03-29 15:09:08 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/pcdiemen/',
+  base: '/003/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
